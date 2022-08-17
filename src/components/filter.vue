52f441c0d383eebac5459ff0831bc6a4f41091a1
Akash Pawar <akashpawar1103@gmail.com>
2022-08-17 21:27:23 +0530

    laptop first commit
    Signed-off-by: Akash Pawar <akashpawar1103@gmail.com>

---
+++ src/components/filter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <h3>jhksjhdkashk</h3>
+  <h3>jhksjhdkashk</h3>
+    <!-- <h1>{{ msg| upper}}</h1> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "filterVue",
+  data() {
+    return {
+    //   msg: "vue filters explained",
+    };
+  },
+};
+</script>
